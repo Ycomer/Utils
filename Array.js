668846a76213e3e28bd6b7086c837193f593e71c
ycomer <dongfangyuan404@gmail.com>
2019-09-17 11:11:41 +0800

    完善 Map

--- Array.js
+++ Array.js
@@ -2,3 +2,19 @@
 function dedupe(array) {
     return Array.from(new Set(array));
 }
+// 其他转化为对象
+function strMapToObj(strMap) {
+    let obj  = Object.create(null);
+    for (let [k,v] of strMap) {
+        obj[k] = v;
+    }
+    return obj;
+}
+// 对象转Map
+function objToStrMap(obj) {
+    let strMap = new Map();
+    for (let k of Object.keys(obj)) {
+        strMap.set(k, obj[k])
+    }
+    return strMap
+}
